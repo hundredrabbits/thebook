cdc5047bbb72d2d53cf2333b496a8c021b8b4132
rekkabell <rekkabell@gmail.com>
2021-02-12 12:38:26 -0800

    *

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@
 
 # ~/Applications/pandoc north_pacific_logbook.md --from markdown+simple_tables+line_blocks --toc -V toc-title:"Table of Contents" --toc-depth=2 --epub-metadata=title.txt --epub-cover-image=img/cover.jpg -w epub -o busy_doing_nothing.epub
 
-~/Applications/pandoc north_pacific_logbook.md --from markdown+simple_tables+line_blocks --toc -V toc-title:"Table of Contents" --toc-depth=2 --epub-metadata=title.txt -epub-cover-image=img/cover.jpg --css epub.css -w epub -o busy_doing_nothing.epub
+~/Applications/pandoc north_pacific_logbook.md --from markdown+simple_tables+line_blocks --toc -V toc-title:"Table of Contents" --toc-depth=2 --epub-metadata=title.txt --epub-cover-image=img/cover.jpg --css epub.css -w epub -o busy_doing_nothing.epub
